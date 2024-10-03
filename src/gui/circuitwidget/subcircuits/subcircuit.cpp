05815ee3690109791581f6957857ce67b4f04694
arcachofo <arcachofo@simulide.com>
2024-10-04 01:40:34 +0200

    Fix nested SubCircuits.

--- src/gui/circuitwidget/subcircuits/subcircuit.cpp
+++ src/gui/circuitwidget/subcircuits/subcircuit.cpp
@@ -42,10 +42,11 @@ Component* SubCircuit::construct( QString type, QString id )
         list.takeLast();
     }
 
-    int rev = MainWindow::self()->revision();
-    if( rev >= 1916 ){ if( name.contains("@") ) list = name.split("@");}
-    else if( name.contains("_") ) list = name.split("_");
-
+    if( Circuit::self()->getSubcircuit() ){
+        int rev = MainWindow::self()->revision();
+        if( rev >= 1916 ){ if( name.contains("@") ) list = name.split("@");}
+        else if( name.contains("_") ) list = name.split("_");
+    }
     if( list.size() > 1 )  // Subcircuit inside Subcircuit: 1_74HC00 to 74HC00
     {
         QString n = list.first();
