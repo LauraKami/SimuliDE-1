629ce985c2fc42ee9d3d8eca55b3ca99730a0a74
arcachofo <arcachofo@simulide.com>
2025-03-12 07:00:47 +0000

    PIC: fix read Port register.

--- src/microsim/mcupin.cpp
+++ src/microsim/mcupin.cpp
@@ -87,8 +87,9 @@ void McuPin::voltChanged()
 
 bool McuPin::getInpState()
 {
-    voltChanged();
-    return m_inpState;
+    //voltChanged();
+    if( m_pinMask ) return IoPin::getInpState();
+    return false;
 }
 
 void McuPin::setPortState( bool state ) // Port Is being witten
